540f4f354b3e7e4460b3e348085cd6dfa51e2223
FelipePereira392 <felipepereiraandrade392@gmail.com>
2025-02-12 17:57:37 -0300

    Finalizado!

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-const input = document.querySelector('#number'), tips = document.querySelector('#tips'), Alert = document.querySelector('#alert')
+//Variables
+
+const input = document.querySelector('#number'), tips = document.querySelector('#tips'), Alert = document.querySelector('#alert'), Reset = document.querySelector('#restart'), Attempt = document.querySelector('#attempts')
 let randomNumber;
+let attempts = 0
+
+//Function NewRandomNumber
 
 const NewRandomNumber = () =>{
     randomNumber = Math.floor(Math.random() * 100) + 1
 }
 NewRandomNumber()
+
+//Principal
+
 document.querySelector('#try').addEventListener('click', () => {
     let value = Number(input.value)
-    input.value = 0
+    input.value = ''
     if (value>100 || value<0){
         Alert.innerHTML = 'Put a valid number (0-100)'
     }else{
+        Attempt.innerHTML = `Attempts: ${attempts += 1}`
         if (value > randomNumber){
-            Alert.innerHTML = 'Less'
+            Alert.innerHTML = `Try a smaller number. <br> (${value} > Secret Number)`
         } else if (value < randomNumber){
-            Alert.innerHTML = 'More'
-        } else {
-            Alert.innerHTML = `Yess, you choose the rigth number (${randomNumber})! Now, have another number for you.`
-            NewRandomNumber()
+            Alert.innerHTML = `Try a bigger number. <br> (${value} < Secret Number)`
+        } else{
+            Alert.style.color = 'green'
+            Attempt.style.color = 'green'
+            Alert.innerHTML = `🥳 You got the correct number ${randomNumber}, reset if you want to play again! 🥳`
+            Reset.style.display = 'block'
         }
     }
+})
+
+//Reset Button
+
+Reset.addEventListener('click', ()=>{
+    Reset.style.display = 'none'
+    attempts = 0
+    Alert.style.color = 'black'
+    Attempt.style.color = 'black'
+    Attempt.innerHTML = `Attempts: ${attempts}`
+    Alert.innerHTML = `Choose a number to start!`
+    NewRandomNumber()
 })
 
 
